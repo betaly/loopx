a87c35c8a16788d18a0e21eb0f65eb3ca31fbab8
TY <towyuan@outlook.com>
2023-08-19 15:39:13 +0800

    chore(enums): add export for TenantStatus from '@loopx/core' (main)

--- services/user-service/src/enums/index.ts
+++ services/user-service/src/enums/index.ts
@@ -1,6 +1,7 @@
 ﻿export * from './auth-provider.enum';
 export * from './user-config-key.enum';
 export * from './permission-key.enum';
-export * from './tenant-status.enum';
 export * from './user-tenant-group.enum';
 export * from './role-type.enum';
+
+export {TenantStatus} from '@loopx/core';
--- services/user-service/src/enums/tenant-status.enum.ts
+++
@@ -1,4 +0,0 @@
-﻿export enum TenantStatus {
-  ACTIVE = 'ACTIVE',
-  INACTIVE = 'INACTIVE',
-}
