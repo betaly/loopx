9af5170686d2f62e83375dc97e2bb4aff9444a96
TY <towyuan@outlook.com>
2023-08-23 21:57:54 +0800

    fix(authentication-service): tenant resolver to return undefined if we can not find by id

--- services/authentication-service/src/providers/tenant-resolver.provider.ts
+++ services/authentication-service/src/providers/tenant-resolver.provider.ts
@@ -20,6 +20,11 @@ export class TenantResolverProvider implements Provider<TenantResolverFn> {
 
   async action(id: string): Promise<Tenant | undefined> {
     // use default tenant if id is not provided
-    return id ? this.tenantRepository.findById(id) : this.getDefaultTenant();
+    if (id) {
+      const tenant = await this.tenantRepository.findOne({where: {id}});
+      if (tenant) {
+        return tenant;
+      }
+    }
   }
 }
